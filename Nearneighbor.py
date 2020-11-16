a7e915d36ab2fa09941d66cd7ea11f063f4e1f5d
henrichsen <41309272+henrichsen@users.noreply.github.com>
2020-11-16 14:02:54 -0700

    add KNN grpahs

--- Nearneighbor.py
+++ Nearneighbor.py
@@ -6,7 +6,7 @@ from scipy.io import loadmat
 import os
 import sys
 import math
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import pandas as pd
 
 
@@ -29,12 +29,29 @@ split = int(.75 * len(p_act))
 
 norm_p = normalize(p_act)
 norm_u_v = normalize(u_v)
-norm_p,norm_u_v=shuffle(norm_p,norm_u_v)
+#norm_p, norm_u_v = shuffle(norm_p, norm_u_v)
 
 weights = ['uniform', 'distance']
 algorithm = ['auto', 'ball_tree', 'kd_tree', 'brute']
 kn = KNeighborsRegressor()
-#get best parameters: distance,auto, k=2 or 3, p=1
+kn.set_params(n_neighbors=2, weights=weights[0], algorithm=algorithm[0], p=1)
+kn.fit(norm_p[:split], norm_u_v[:split])
+prediction = kn.predict(norm_p[split:])
+
+plt.plot(norm_u_v[split:, 0])
+plt.plot(prediction[:, 0])
+# plt.plot(norm_u_v[split:,1])
+# plt.plot(prediction[0,1])
+
+plt.grid()
+plt.legend(['actual u', 'predicted u', 'actual v', 'predicted v'])
+plt.show()
+plt.plot(norm_u_v[split:, 1])
+plt.plot(prediction[:, 1])
+plt.legend(['actual v', 'predicted v'])
+plt.show()
+
+# get best parameters: distance,auto, k=2 or 3, p=1
 # for weight in weights:
 #     print(weight)
 #     for alg in algorithm:
